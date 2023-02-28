7043c292034671f7d79a85e57f9fd7a8bd0acd5f
DanielCu <danielcu@alternet.com.mx>
2023-02-28 08:44:01 -0600

    ejercicio 2

--- ejercicio_2/ejercicio_2.py
+++ ejercicio_2/ejercicio_2.py
@@ -1,5 +1,7 @@
 #Daniel Cu Sánchez - A01703613 - 23-02-23
 from matplotlib import pyplot as plt #Library for graphs
+import pandas as pd
+import math
 import numpy as np #Library for managing data
 # Input number
 number = int(input("Choose a number\n1) data01.txt\n2) data02.txt\n3) data03.txt\nType:"))
@@ -8,16 +10,24 @@ number = int(input("Choose a number\n1) data01.txt\n2) data02.txt\n3) data03.txt
 match number:
   case 1:
     file = "data01.txt"
-    intervals=11
   case 2:
     file = "data02.txt"
-    intervals=11
   case 3:
     file = "data03.txt"
-    intervals=7
 # Load array
 array_data = np.loadtxt(file, dtype=float)
-counts, edges, bars= plt.hist(array_data, bins=intervals, alpha=0.7, edgecolor='k', linewidth=1)
+
+classes = math.ceil(1 + (3.3 * math.log10(array_data.size)))
+w = array_data.size / classes
+print("c = ",classes)
+print("max = ",array_data.max())
+print("min = ",array_data.min())
+print("w = ", (array_data.max() - array_data.min()) / classes) #data - menor / clasess
+table = np.histogram(array_data,classes)
+
+n, bins, bars= plt.hist(array_data, bins=classes, alpha=0.7, edgecolor='k', linewidth=1)
+print("table = ", bins)
+print("sum of frequencies = ",  np.array(n).sum())
 # Print histogram
 plt.title("Histogram")
 plt.xlabel("Intervals")
